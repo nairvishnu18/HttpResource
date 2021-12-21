ca10306c9a870e7c5bd7aa63518ed1351e37c029
Vishnu Nair <vishnunair>
2021-12-21 12:41:23 +0530

    Added Comments and Error type

--- Sources/HttpResource/HttpResource.swift
+++ Sources/HttpResource/HttpResource.swift
@@ -5,7 +5,12 @@ public struct HttpResource {
     
     @available(macOS 10.15.0, *)
     @available(macOS 12.0, *)
-    
+
+    /// Generic Async function to decode json data from given url request.
+    ///  - Parameters:
+    ///   - urlRequest: url of the server api
+    ///   - responseType: Type of model by which json data is to be decoded
+    ///  - Returns: Decoded json data
     public func getJsonData<T: Decodable>(from urlRequest: URLRequest, forResponseType responseType: T.Type) async throws -> T {
         
         do {
